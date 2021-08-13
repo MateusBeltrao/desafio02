56d989e1b7584e527cd98e0a473ca99ab0757f50
Mateus Beltrão <mateusbeltrao2019@gmail.com>
2021-08-13 19:51:49 -0300

    desafio 0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const [formValues, setFormValues] = useState ({})
+
+    const handleInputChange = (e) =>{
+      const {name, value} = e.target
+      setFormValues({...formValues, [name]: value})
+    }
+    
+    const handleSubmit = (e) =>{
+      e.placeholder()
+      const formData = new FormData(e.target)
+      const data = Object.fromEntries(formData)
+      console.log(data)
+    }
+  
+    console.log(formValues)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <form onSubmit={handleSubmit}>
+      <header>Email</header>
+      <input type="text" name="email" placeholder="email@email.com" onChange={handleInputChange} value={formValues.email || ''}></input>
+      <header>Confirme o email</header>
+      <input type="text" name="confirmaEmail" placeholder="email@email.com" onChange={handleInputChange} value={formValues.confirmaEmail || ''}></input>
+      <header>Nome</header>
+      <input type="text" name="nome" placeholder="nome" onChange={handleInputChange} value={formValues.nome || ''}></input>
+      <header>Sobrenome</header>
+      <input type="text" name="nome" placeholder="sobrenome" onChange={handleInputChange} value={formValues.sobrenome || ''}></input>
+      <header></header>CPF
+      <input type="text" name="cpf" placeholder="cpf" onChange={handleInputChange} value={formValues.cpf || ''}></input>
+
+    {/*  
+      <select name="tipos">
+        <option value="celular">celular</option>
+        <option value="telefone">telefone</option>
+      </select>
+    */}
+      
+      
+      <div className="radio">
+        <input type="radio" value="1" name="gênero"/>Radio Button1
+        <input type="radio" value="2" name="gênero"/>Radio Button2
+        <input type="radio" value="3" name="gênero"/>Radio Button3
+        </div>
+        
+
+      <button type="submit">Concluir</button>
+    </form>
   );
 }
 
